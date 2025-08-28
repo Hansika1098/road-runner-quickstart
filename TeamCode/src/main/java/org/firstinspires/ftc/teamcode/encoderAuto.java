6dedfdd00f3bdb725903ee78576604df0c348a10
shreyasriks <shreyasriks@gmail.com>
2025-08-27 19:07:09 -0700

    hi

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/encoderAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/encoderAuto.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
-
+//hello hihihi
 @Autonomous
 public class encoderAuto extends LinearOpMode{
     public void runOpMode() throws InterruptedException {
