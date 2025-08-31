a30b3b0152e4d843674540000d75fdffe13301b4
shreyasriks <shreyasriks@gmail.com>
2025-08-31 16:50:42 -0700

    actions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmTestOG.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmTestOG.java
@@ -1,25 +1,29 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.acmerobotics.dashboard.config.Config;
+import com.arcrobotics.ftclib.controller.PIDController;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.ejml.dense.row.decomposition.qr.QRColPivDecompositionHouseholderColumn_DDRM;
+
 @TeleOp
 @Config
-public class ArmTest extends LinearOpMode {
+public class ArmTestOG extends LinearOpMode {
 
     public static int target = 0;
     public static double multiplier = 0.005;
     private ElapsedTime timer = new ElapsedTime();
 
-    double currentTime = 0;
-    double p;
-    double d;
-    double power = 0;
+    double ticksPerDegrees = 2.0;
 
-    double lastTime = 0;
+    public static double p = 0, i = 0,  d = 0, f = 0;
+
+    public PIDController controller  = new PIDController(p,i,d);
+
 
 
     @Override
@@ -27,31 +31,21 @@ public class ArmTest extends LinearOpMode {
 
     public void runOpMode() throws InterruptedException {
         DcMotorEx arm = hardwareMap.get(DcMotorEx.class, "arm");
+        arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         waitForStart();
-        timer.reset();
 
-        int distToTarget;
 
-        distToTarget = target - arm.getCurrentPosition();
 
-        int prevDist = distToTarget;
         while(opModeIsActive()){
-            distToTarget = target - arm.getCurrentPosition();
-            p = distToTarget * multiplier;
-            d = (distToTarget - prevDist)/(currentTime - lastTime);
+            controller.setPID(p,i,d);
+            double pid = controller.calculate(arm.getCurrentPosition(), target);
+            double ff = Math.cos(Math.toRadians(target / ticksPerDegrees))*f;
 
-
-
-            power = d + p;
-
-            arm.setPower(power);
+            arm.setPower(pid + ff);
 
             telemetry.addData("ArmPos", arm.getCurrentPosition());
             telemetry.update();
-            prevDist = distToTarget;
-            lastTime = currentTime;
-            currentTime = timer.milliseconds();
 
 
 
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestingAction.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode;
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.NonConst;
+
+
+public class TestingAction extends LinearOpMode{
+
+    ElapsedTime time = new ElapsedTime();
+    public MecanumDrive drive;
+
+
+    @TeleOp
+    public class MoveForward implements Action {
+        private boolean init = false;
+        @Override
+        public boolean run(@NonNull TelemetryPacket packet) {
+
+            if(!init) {
+                time.reset();
+                drive.rightFront.setPower(0.5);
+                drive.leftFront.setPower(0.5);
+                drive.rightBack.setPower(0.5);
+                drive.leftBack.setPower(0.5);
+                init = true;
+
+            }
+            if(time.milliseconds() < 3000) {
+                return true;
+
+            }
+            drive.rightFront.setPower(0);
+            drive.leftFront.setPower(0);
+            drive.rightBack.setPower(0);
+            drive.leftBack.setPower(0);
+            return false;
+        }
+
+    }
+
+}
+
