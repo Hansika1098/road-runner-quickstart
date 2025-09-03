3cd2708e2747a4095ee09f0649adf80b1e5ac669
shreyasriks <shreyasriks@gmail.com>
2025-09-02 21:09:36 -0700

    actions in roadrunner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestingAction.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestingAction.java
@@ -1,22 +1,67 @@
 package org.firstinspires.ftc.teamcode;
 import androidx.annotation.NonNull;
 
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.InstantAction;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.SleepAction;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.util.ElapsedTime;
+
 
 import org.firstinspires.ftc.robotcore.external.NonConst;
 
+import java.util.*;
 
+@TeleOp
 public class TestingAction extends LinearOpMode{
+    private FtcDashboard dash = FtcDashboard.getInstance();
+    private List<Action> runningActions = new ArrayList<>();
 
     ElapsedTime time = new ElapsedTime();
     public MecanumDrive drive;
 
 
-    @TeleOp
+    @Override
+    public void runOpMode() throws InterruptedException {
+        drive = new MecanumDrive(hardwareMap, new Pose2d(0,0,0));
+
+        waitForStart();
+
+
+        while(opModeIsActive()) {
+            TelemetryPacket packet = new TelemetryPacket();
+
+            if (gamepad1.a) {
+                runningActions.add(MoveForward());
+            }
+
+            // updated based on gamepads
+
+            // update running actions
+            List<Action> newActions = new ArrayList<>();
+            for (Action action : runningActions) {
+                action.preview(packet.fieldOverlay());
+                if (action.run(packet)) {
+                    newActions.add(action);
+                }
+            }
+            runningActions = newActions;
+
+            dash.sendTelemetryPacket(packet);
+
+        }
+
+
+    }
+
+
     public class MoveForward implements Action {
         private boolean init = false;
         @Override
@@ -33,7 +78,6 @@ public class TestingAction extends LinearOpMode{
             }
             if(time.milliseconds() < 3000) {
                 return true;
-
             }
             drive.rightFront.setPower(0);
             drive.leftFront.setPower(0);
@@ -43,6 +87,8 @@ public class TestingAction extends LinearOpMode{
         }
 
     }
+
+    public Action MoveForward() {return new MoveForward(); }
 
 }
 
