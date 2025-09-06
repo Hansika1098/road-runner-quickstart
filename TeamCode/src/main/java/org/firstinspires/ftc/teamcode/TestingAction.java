d5fda88444efb3d6b9256f6a290297c4285b726d
Hansika1098 <hansika.varanasi@gmail.com>
2025-09-05 20:05:28 -0700

    updated mecanum drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestingAction.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestingAction.java
@@ -1,22 +1,49 @@
 package org.firstinspires.ftc.teamcode;
 import androidx.annotation.NonNull;
 
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.SleepAction;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.NonConst;
 
+import java.util.*;
 
+@TeleOp
 public class TestingAction extends LinearOpMode{
-
+    private FtcDashboard dash = FtcDashboard.getInstance();
+    private List<Action> runningActions = new ArrayList<>();
     ElapsedTime time = new ElapsedTime();
     public MecanumDrive drive;
 
+    @Override
+    public void runOpMode() throws InterruptedException {
+        TelemetryPacket packet = new TelemetryPacket();
 
-    @TeleOp
+        if (gamepad1.a) {
+            runningActions.add(MoveForward());
+        }
+        // updated based on gamepads
+
+        // update running actions
+        List<Action> newActions = new ArrayList<>();
+        for (Action action : runningActions) {
+            action.preview(packet.fieldOverlay());
+            if (action.run(packet)) {
+                newActions.add(action);
+            }
+        }
+        runningActions = newActions;
+
+        dash.sendTelemetryPacket(packet);
+    }
+
+
     public class MoveForward implements Action {
         private boolean init = false;
         @Override
@@ -43,6 +70,8 @@ public class TestingAction extends LinearOpMode{
         }
 
     }
+
+    public Action MoveForward() { return new MoveForward(); }
 
 }
 
